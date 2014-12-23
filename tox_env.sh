5aeeb2031a19026b8ea06430bf131c67e1fa7c26
Pete Moore <pmoore@mozilla.com>
2014-12-23 14:10:25 +0100

    stuff

--- tox_env.sh
+++ tox_env.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -e
-[ -z "${1}" ] && exit 1
-TOX_DIR="${1}"
+[ -z "${1}" ] || [ -z "${2}" ] && exit 1
+TOX_INI_DIR="${1}"
+TOX_WORK_DIR="${1}"
 
 function hgme {
     repo="${1}"
-    if [ ! -d "${TOX_DIR}/${repo}" ]; then
-        hg clone https://hg.mozilla.org/build/${repo} "${TOX_DIR}/${repo}"
+    if [ ! -d "${TOX_WORK_DIR}/${repo}" ]; then
+        hg clone https://hg.mozilla.org/build/${repo} "${TOX_WORK_DIR}/${repo}"
     else
         # this is equivalent to hg purge but doesn't require the hg purge plugin to be enabled
-        hg status -un0 -R "${TOX_DIR}/${repo}" | xargs rm -rf
-        hg pull -u -R "${TOX_DIR}/${repo}"
+        hg status -un0 -R "${TOX_WORK_DIR}/${repo}" | xargs rm -rf
+        hg pull -u -R "${TOX_WORK_DIR}/${repo}"
     fi
 }
 
@@ -17,5 +18,8 @@ hgme tools
 hgme buildbotcustom
 hgme buildbot
 
-hg -R "${TOX_DIR}/buildbot" checkout production-0.8
-cd "${TOX_DIR}/buildbot/master" && python setup.py install
+hg -R "${TOX_WORK_DIR}/buildbot" checkout production-0.8
+cd "${TOX_WORK_DIR}/buildbot/master" && python setup.py install
+rm -rf "${TOX_INI_DIR}/test-output"
+rm -rf "${TOX_INI_DIR}/run/shm/buildbot"
+mkdir -p "${TOX_INI_DIR}/run/shm/buildbot"
